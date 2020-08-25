84d19dbfe5ef40147d90c6b604c34006b452d833
kodir-zaelani <kodir.zaelani78@gmail.com>
2020-08-26 07:16:04 +0800

    add participant

--- database/migrations/2020_08_25_224913_create_participants_table.php
+++ database/migrations/2020_08_25_224913_create_participants_table.php
@@ -15,7 +15,29 @@ class CreateParticipantsTable extends Migration
     {
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('event_id')->unsigned();
+            $table->string('nik');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('birthplace');
+            $table->text('dateofbirth');
+            $table->string('image')->nullable();
+            $table->string('no_hp');
+            $table->string('no_wa');
+            $table->string('email')->unique();
+            $table->string('jabatan_dpc');
+            $table->boolean('status_dprd')->default(false);
+            $table->string('jabatan_dprd');
+            $table->string('rt');
+            $table->string('district');
+            $table->string('vilage');
+            $table->string('city');
+            $table->string('postalcode');
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('restrict');
+
         });
     }
 
